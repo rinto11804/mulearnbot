99b8a8ca6b6d7428cc27254ab49851c6724d386b
rinto11804 <rinto11804@gmail.com>
2024-05-05 11:15:00 +0530

    feat(app): task 1 completed
    When a user joins bot greets in welcomes channel and in inbox of user

--- welcome.py
+++ welcome.py
@@ -1,5 +1,6 @@
 from typing import Final
 import discord
+from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
@@ -26,6 +27,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    
+            
 
     if message.content.startswith("$hello"):
         await message.reply("Hello!", mention_author=True)
@@ -34,10 +38,21 @@ async def on_message(message):
 @client.event
 async def on_member_join(member: discord.Member):
     guild = member.guild
-    welcome_channel = client.get_channel(WELCOME_CHANNEL_ID)
+
+    # sending welcome message to welocome channel
+    welcome_channel = client.get_channel(int(WELCOME_CHANNEL_ID))
     if welcome_channel is not None:
-        to_send = f"Welcome {member.mention} to {guild.name}!"
-        await welcome_channel.send(to_send)
+        to_send = f"Welcome {member._user.name} to {guild.name}!"
+        try:
+            await welcome_channel.send(to_send,mention_author=True)
+        except Exception as e:
+            print(e)
+    
+    # sending welcome message to users inbox
+    try:
+        await member.send(to_send,mention_author=True)
+    except Exception as e:
+        print(e)
 
 
 client.run(token=TOKEN)
