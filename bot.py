9c565814d02d8e5a577e07737dc0f57c11805e8d
rinto11804 <rinto11804@gmail.com>
2024-05-06 18:05:03 +0530

    handled error in bot.py

--- bot.py
+++ bot.py
@@ -27,11 +27,6 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 
 
 @bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user} ID: {bot.user.id}")
-
-
-@bot.event
 async def on_message(message: discord.Message):
     if message.author == bot.user:
         return
@@ -46,26 +41,30 @@ async def on_message(message: discord.Message):
             )
         connection.commit()
     except Exception as e:
-        print(e)
+        print("failed to add user word")
 
 
 @bot.tree.command(name="word-status", description="Gives the 10 most used words.")
 async def word_status(interaction: discord.Interaction):
-    cursor = connection.cursor()
-    cursor.execute(
-        """SELECT word, count(*) AS count
-            FROM (
-            SELECT lower(word) AS word
-            FROM user_word
-            ) AS word_counts
-            GROUP BY word
-            ORDER BY count DESC
-            LIMIT 10;"""
-    )
-    rows = cursor.fetchall()
-    connection.commit()
-    res = "\n".join([f"{row[0]}: {row[1]}" for row in rows])
-    await interaction.response.send_message(res, ephemeral=True)
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT word, count(*) AS count
+                FROM (
+                SELECT lower(word) AS word
+                FROM user_word
+                ) AS word_counts
+                GROUP BY word
+                ORDER BY count DESC
+                LIMIT 10;"""
+        )
+        rows = cursor.fetchall()
+        connection.commit()
+        res = "\n".join([f"{row[0]}: {row[1]}" for row in rows])
+        await interaction.response.send_message(res, ephemeral=True)
+    except Exception as e:
+        print("Failed to load word status")
+        await interaction.response.send_message("Failed to load word status")
 
 
 @bot.tree.command(
@@ -73,22 +72,27 @@ async def word_status(interaction: discord.Interaction):
     description="Gives the 10 most used words by the specified user.",
 )
 async def word_status(interaction: discord.Interaction, user: discord.Member):
-    cursor = connection.cursor()
-    cursor.execute(
-        """SELECT word, count(*) AS count
-            FROM (
-            SELECT lower(word) AS word
-            FROM user_word WHERE discord_id = '%s'
-            ) AS word_counts
-            GROUP BY word
-            ORDER BY count DESC
-            LIMIT 10;""",
-        (user.id,),
-    )
-    rows = cursor.fetchall()
-    connection.commit()
-    res = "\n".join([f"{row[0]}: {row[1]}" for row in rows])
-    await interaction.response.send_message(res, ephemeral=True)
+    try:
+        cursor = connection.cursor()
+        cursor.execute(
+            """SELECT word, count(*) AS count
+                FROM (
+                SELECT lower(word) AS word
+                FROM user_word WHERE discord_id = '%s'
+                ) AS word_counts
+                GROUP BY word
+                ORDER BY count DESC
+                LIMIT 10;""",
+            (user.id,),
+        )
+        rows = cursor.fetchall()
+        connection.commit()
+        res = "\n".join([f"{row[0]}: {row[1]}" for row in rows])
+        await interaction.response.send_message(res, ephemeral=True)
+    except Exception as e:
+        await interaction.response.send_message(
+            "Failed to load use status", ephemeral=True
+        )
 
 
 @bot.tree.command(name="select-role", description="select from available roles")
